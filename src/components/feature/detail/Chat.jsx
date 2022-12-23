bfe8372f53390247d14248aa4ebcc4c3fa25b6b0
Kim <kimmy199535@gmail.com>
2022-12-23 20:02:51 +0900

    🎨Design: 로그인, 회원가입, 채팅 페이지 기본 레이아웃 설정

--- src/components/feature/detail/Chat.jsx
+++ src/components/feature/detail/Chat.jsx
@@ -1,1 +1,58 @@
-1111111;
+import React from "react";
+import styled from "styled-components";
+
+function Chat() {
+  return (
+    <StTopContainer>
+      <StBorder>
+        <StChatBorder></StChatBorder>
+        <hr></hr>
+        <StBottomBorder>
+          <div>
+            <StText></StText>
+          </div>
+          <div>
+            <button>전송</button>
+          </div>
+        </StBottomBorder>
+      </StBorder>
+    </StTopContainer>
+  );
+}
+
+export default Chat;
+
+const StTopContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 50px;
+
+  gap: 50px;
+`;
+
+const StBorder = styled.div`
+  border: 1px solid #484848;
+  height: 600px;
+  width: 800px;
+`;
+
+const StChatBorder = styled.div`
+  height: 450px;
+  width: 800px;
+`;
+
+const StBottomBorder = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  gap: 20px;
+  height: 150px;
+  width: 800px;
+`;
+
+const StText = styled.textarea`
+  height: 100px;
+  width: 650px;
+`;
--- src/components/feature/login/Signup.jsx
+++ src/components/feature/login/Signup.jsx
@@ -1,1 +1,68 @@
-1111111;
+import React from "react";
+import styled from "styled-components";
+import Banner from "../../../assets/img/line_banner.png";
+
+function Signup() {
+  return (
+    <StTopContainer>
+      <div>
+        <img alt="banner" src={Banner} height="150px" />
+      </div>
+      <StInputGroup>
+        <div>
+          <input placeholder="이메일"></input>
+        </div>
+        <div>
+          <input placeholder="닉네임"></input>
+        </div>
+        <div>
+          <input placeholder="비밀번호"></input>
+        </div>
+      </StInputGroup>
+      <StButtonGroup>
+        <div>
+          <button>회원가입</button>
+        </div>
+        <StLink>
+          <a href="/login">이메일로 로그인</a>
+        </StLink>
+      </StButtonGroup>
+    </StTopContainer>
+  );
+}
+
+export default Signup;
+
+const StTopContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 100px;
+
+  gap: 50px;
+`;
+
+const StInputGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: auto;
+`;
+
+const StButtonGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  margin: auto;
+  gap: 20px;
+`;
+
+const StLink = styled.div`
+  a {
+    font-weight: 400;
+    text-decoration: none;
+    color: #4d4f50;
+  }
+`;
