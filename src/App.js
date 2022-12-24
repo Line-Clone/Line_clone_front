407bd8d75f3faf6b365b1f3c540bf80a8abd7c6e
Kim <kimmy199535@gmail.com>
2022-12-24 20:00:39 +0900

    ✨Feat: 회원가입 정규표현식 추가

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import React from "react";
 import Router from "./shared/Router";
 
 function App() {
--- src/components/feature/login/Login.jsx
+++ src/components/feature/login/Login.jsx
@@ -2,30 +2,55 @@ import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import Banner from "../../../assets/img/line_banner.png";
+import { useForm } from "react-hook-form";
 
 function Login() {
+  const { register, handleSubmit, getValues } = useForm();
+
+  const onValid = (data) => {
+    console.log(data);
+  };
+
+  const { email, password } = getValues();
+
   return (
-    <StTopContainer>
-      <div>
-        <img alt="banner" src={Banner} height="150px" />
-      </div>
-      <StInputGroup>
+    <form onSubmit={handleSubmit(onValid)}>
+      <StTopContainer>
         <div>
-          <input placeholder="이메일"></input>
+          <img alt="banner" src={Banner} height="150px" />
         </div>
-        <div>
-          <input placeholder="비밀번호"></input>
-        </div>
-      </StInputGroup>
-      <StButtonGroup>
-        <div>
-          <button>로그인</button>
-        </div>
-        <Link to="/signup">
-          <button>신규 가입</button>
-        </Link>
-      </StButtonGroup>
-    </StTopContainer>
+        <StInputGroup>
+          <div>
+            <input
+              {...register("email", { required: "이메일은 필수 입력입니다." })}
+              id="email"
+              type="email"
+              name="email"
+              placeholder="이메일"
+            ></input>
+          </div>
+          <div>
+            <input
+              {...register("password", {
+                required: "비밀번호는 필수 입력입니다.",
+              })}
+              id="password"
+              type="password"
+              name="password"
+              placeholder="비밀번호"
+            ></input>
+          </div>
+        </StInputGroup>
+        <StButtonGroup>
+          <div>
+            <button type="submit">로그인</button>
+          </div>
+          <Link to="/signup">
+            <button>신규 가입</button>
+          </Link>
+        </StButtonGroup>
+      </StTopContainer>
+    </form>
   );
 }
 
